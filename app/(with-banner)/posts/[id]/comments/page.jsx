f092a1b3a402bdf8eaefeb7656762e53398b5bd5
Alex Stefano Falcon Flores <alexfalconflores@hotmail.com>
2022-11-29 22:43:14 -0500

    [ADD] new next image

--- app/(with-banner)/posts/[id]/comments/page.jsx
+++ app/(with-banner)/posts/[id]/comments/page.jsx
@@ -1,14 +1,16 @@
+import Image from "next/image"
+
 const fetchComments = async (id) => {
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-  throw new Error('Error al cargar los comentarios')
+  // await new Promise((resolve) => setTimeout(resolve, 3000))
+  // throw new Error('Error al cargar los comentarios')
 
   // incremental static regeneration
-  // return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
-  //   next: {
-  //     revalidate: 60
-  //   }
-  // })
-  //   .then(res => res.json())
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`, {
+    next: {
+      revalidate: 60
+    }
+  })
+    .then(res => res.json())
 }
 
 export default async function Post ({ params }) {
@@ -19,6 +21,7 @@ export default async function Post ({ params }) {
     <ul style={{ background: '#181818' }}>
       {comments.map(comment => (
         <li key={comment.id}>
+          <Image width={50} height={50} alt={comment.name} src={`https://avatars.dicebear.com/api/avataaars/${comment.email}.svg`} />
           <h4>{comment.name}</h4>
           <small>{comment.body}</small>
         </li>
